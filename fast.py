48f7a36053f5e51d48577dd3653273cba05fd5de
padel1 <adelzeghad11@gmail.com>
2024-09-10 00:47:20 +0100

    fix

--- fast.py
+++ fast.py
@@ -1,3 +1,5 @@
+import csv
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -31,14 +33,25 @@ class Massage(TypedDict):
     
 
 ar_en = r"C:\wingo\models--wingo-dz--ar-to-en-v6-optimized\snapshots\43d52422a0ebaacaeddac545d7db732cbc905bcb"
+darija = r"C:\wingo\models--wingo-dz--darija_test\snapshots\1680c9e23fa54799b0f6627eed6dad5eaa8407a7"
 # en_ar = "/home/ali/Desktop/translation_models/models--wingo-dz--en-to-ar-v6-optimized/snapshots/54d44df920564fbd4a6f033c71aeb0a302cf7aa8"
 # fr_en = "/home/ali/Desktop/translation_models/models--wingo-dz--fr-to-en-v3-optimized/snapshots/cd9cefdf35248c5379918aa8074c5317853bdd63"
 # en_fr = "/home/ali/Desktop/translation_models/models--wingo-dz--en-to-fr-v3-optimized/snapshots/e36ec621c390ca82cb2a0e47015bab070425a93f"
 
-
+def save_to_csv(en_text, ar_text):
+    file_exists = os.path.isfile('translations.csv')
+    
+    with open('translations.csv', mode='a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(['English', 'Arabic'])  # Write header if file does not exist
+        writer.writerow([en_text, ar_text])  # Write the English and Arabic translations
 
 translator_ar_en = ctranslate2.Translator(ar_en)
 tokenizer_ar_en = transformers.AutoTokenizer.from_pretrained(ar_en)
+
+translator_darija = ctranslate2.Translator(darija)
+tokenizer_darija = transformers.AutoTokenizer.from_pretrained(darija)
 
 # translator_en_ar = ctranslate2.Translator(en_ar)
 # tokenizer_en_ar = transformers.AutoTokenizer.from_pretrained(en_ar)
@@ -59,7 +72,21 @@ async def root(massage: Massage):
     target = results[0].hypotheses[0]
 
     response = tokenizer_ar_en.decode(tokenizer_ar_en.convert_tokens_to_ids(target))
+    save_to_csv(massage["msg"], response)
+    return {"response": response}
+
+
+@app.post("/darija")
+async def root(massage: Massage):
+    print(massage["msg"])
+    source = tokenizer_darija.convert_ids_to_tokens(tokenizer_darija.encode(massage["msg"]))
+
+    results = await asyncio.to_thread(translator_darija.translate_batch, [source])
     
+    target = results[0].hypotheses[0]
+
+    response = tokenizer_darija.decode(tokenizer_darija.convert_tokens_to_ids(target))
+    save_to_csv(massage["msg"], response)
     return {"response": response}
  
 
--- translation_app.py
+++ translation_app.py
@@ -15,21 +15,12 @@ def translate(text, model):
     else:
         return "Error in translation request"
 
-# Function to save translations to CSV
-def save_to_csv(en_text, ar_text):
-    file_exists = os.path.isfile('translations.csv')
-    
-    with open('translations.csv', mode='a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        if not file_exists:
-            writer.writerow(['English', 'Arabic'])  # Write header if file does not exist
-        writer.writerow([en_text, ar_text])  # Write the English and Arabic translations
 
 # Streamlit UI
 st.title("Translation Model Tester")
 
 # Model selection
-model = st.selectbox("Choose a model:", ("ar_en", "en_ar", "fr_en", "en_fr"))
+model = st.selectbox("Choose a model:", ("ar_en","darija", "en_ar", "fr_en", "en_fr"))
 
 # Text input
 text_input = st.text_area("Enter text to translate:")
@@ -41,11 +32,7 @@ if st.button("Translate"):
         st.write("**Result:**")
         st.success(result)
         
-        # Save translations to CSV
-        if model in ["ar_en", "en_ar"]:
-            en_text = text_input if model == "ar_en" else result
-            ar_text = result if model == "ar_en" else text_input
-            save_to_csv(en_text, ar_text)
+        
         
     else:
         st.warning("Please enter text to translate.")
